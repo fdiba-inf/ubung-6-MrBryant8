f9512a26f5c917b6261139129f860623e0bc96e1
MrBryant8 <mincho.tanev@fdiba.tu-sofia.bg>
2021-11-17 18:12:00 +0200

    wawawawa

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -5,5 +5,18 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int number = input.nextInt();
+
+        int reversednumber = 0;
+        int tempnum = number;
+        while (tempnum > 0) {
+            int nextDigit = tempnum%10;
+            reversednumber=reversednumber*10 + nextDigit;
+            tempnum=tempnum/10;
+        }
+        boolean palindrome= number ==reversednumber;
+        System.out.println("Palindrome: " + palindrome);
     }
 }
--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -5,5 +5,47 @@ import java.util.Scanner;
 public class PasswordCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        boolean PasswordValid=false;
+        do {
+            System.out.println("Enter a password: ");
+            String password= input.nextLine();
+
+            if(password.length()<8){
+                continue;
+            }
+
+            boolean AllLettersAndDigits=true;
+
+            for (int i = 0; i < password.length(); i++) {
+                char symbol = password.charAt(i);
+                boolean LetterOrDigits = Character.isLetterOrDigit(symbol);
+                if (!LetterOrDigits) {
+                    AllLettersAndDigits = false;
+                    break;
+                }
+                int digitCount=0;
+                boolean Digit=Character.isDigit(symbol);
+                if(Digit){
+                    digitCount++;
+                }
+                int digitCount=0;
+                boolean Digit=Character.isDigit(symbol);
+                if(Digit){
+                    digitCount++;
+                }
+            }
+
+            int digitCount=0;
+            boolean Digit=Character.isDigit(symbol);
+            if(Digit){
+                digitCount++;
+            }
+
+            if (AllLettersAndDigits && digitCount>=2){
+                PasswordValid=true;
+                System.out.println("Password Valid!");
+            }
+
+        }while(!PasswordValid);
     }
 }
